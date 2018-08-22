774b4b300ab1b5e2dc39c9f241f9a5217718a531
adimitrani <adi.mitrani4@gmail.com>
2018-08-22 17:41:57 +0300

    Basic http server is working, login page not working

---
+++ code/src/JavaHttpServer.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class JavaHttpServer {
+
+	public static void main(String args[]){
+		int port = 8080;
+		try {
+			HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+			System.out.println("server started at " + port);
+			server.createContext("/", new RootHandler());
+			server.createContext("/login", new LoginHandler());
+			//server.createContext("/echoHeader", new EchoHeaderHandler());
+			//server.createContext("/echoGet", new EchoGetHandler());
+			//server.createContext("/echoPost", new EchoPostHandler());
+			server.setExecutor(null);
+			server.start();
+		}
+		catch (IOException e){
+
+		}
+	}
+}
